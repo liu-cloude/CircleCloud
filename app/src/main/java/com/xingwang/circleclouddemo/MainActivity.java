ebb2c0125aad890f93a0ed0c7e70810ec328065a
liumz <1078325948@qq.com>
2019-12-09 15:34:50 +0800

    文章模块增加混淆

--- app/src/main/java/com/xingwang/circleclouddemo/MainActivity.java
+++ app/src/main/java/com/xingwang/circleclouddemo/MainActivity.java
@@ -6,23 +6,34 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.xingwang.circle.CircleActivity;
+import com.xingwang.essay.EssayListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     protected TextView tv_jump;
-
+    protected TextView tv_jump_essay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tv_jump=findViewById(R.id.tv_jump);
+        tv_jump_essay=findViewById(R.id.tv_jump_essay);
 
         tv_jump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CircleActivity.getIntent(MainActivity.this);
+                //HttpU
             }
         });
 
+        tv_jump_essay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EssayListActivity.getIntent(MainActivity.this);
+                //HttpU
+            }
+        });
     }
 }
